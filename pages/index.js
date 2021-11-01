025f79b448210b922816656da2edd00e5b5df4d5
Tudor Alexandru <tudorale2005@gmail.com>
2021-11-01 22:04:40 +0200

    updates

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,8 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Navbar from '../components/Navbar/Navbar'
 import styles from '../styles/Home.module.css'
-
+import Link from "next/link"
 export default function Home() {
   return (
     <div className={styles.container}>
@@ -11,59 +12,99 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+      <Navbar/>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+      <div className={styles.hero}>
+        <div className={styles.leftHero}>
+          <div className={styles.heroContent}>
+            <h1>Don&apos;t let<br/>Your Memes</h1>
+            <h2>Be Dreams</h2>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+            <div className={styles.buttons}>
+              <Link passHref href="/airdrop"><p>AirDrop <b>IN PROGRESS</b></p></Link>
+              <Link passHref href="/memepaper"><p>MemePaper Coming Soon!</p></Link>
+            </div>
+          </div>
+          <div className={styles.addon}></div>
         </div>
-      </main>
+        <div className={styles.rightHero}>
+          <img src="/illustration.png" alt="illustration"/>
+        </div>
+      </div>
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
+      <div className={styles.phases}>
+        <div className={styles.phaseOne}>
+          <p className={styles.upper}>Phase 1</p>
+          <div className={styles.down}>
+            <h4>MemeMarketplace Exchange</h4>
+            <p>Unite ALL Meme Tokens and NFTs in ONE place</p>
+          </div>
+        </div>
+        <div>
+          <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M5 16H27" stroke="#D21C0E" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+            <path d="M18 7L27 16L18 25" stroke="#D21C0E" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+          </svg>
+        </div>
+        <div className={styles.phaseTwo}>
+          <p className={styles.upper}>Phase 2</p>
+          <div className={styles.down}>
+            <p>Normie Integration into the Solana NFT Ecosystem</p>
+          </div>
+        </div>
+      </div>
+
+      <div className={styles.sectionOne}>
+        <div className={styles.sectionContent}>
+          <h1>How will we get the Normies?</h1>
+          <div className={styles.sectionGrid}>
+            <div className={styles.sectionLeft}>
+              <Image alt="meme marketplace" width="600" height="340" src="/innerImage.png"/>
+            </div>
+            <div className={styles.sectionRight}>
+              <div className={styles.criteria}>
+                <div className={styles.number}>1</div>
+                <p>Influencers are carefully selected based on specific criteria we deem worthy of our platform, using an exclusive Invite ONLY business model</p>
+              </div>
+
+              <div className={styles.criteria}>
+                <div className={styles.number}>2</div>
+                <p>MemeMarketplace offers an integrated A-Z solution allowing influencers the privilege of working One on One with our established team of Artists and Developers, giving Life to their ideas</p>
+              </div>
+
+              <div className={styles.criteria}>
+                <div className={styles.number}>3</div>
+                <p>Influencers inform their followers of their creation and upcoming sale</p>
+              </div>
+
+              <div className={styles.criteria}>
+                <div className={styles.number}>4</div>
+                <p>MemeMarketplace serves as the<br/> Normie NFT On-Ramp</p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <svg width="127" className={styles.sectionDecoration} height="269" viewBox="0 0 127 269" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M250.165 152.914C260.412 88.9564 216.872 28.8015 152.914 18.5542C88.9563 8.30704 28.8014 51.8479 18.5542 115.806C8.30701 179.763 51.8479 239.918 115.806 250.165C179.763 260.413 239.918 216.872 250.165 152.914Z" stroke="white" strokeWidth="3" strokeMiterlimit="10" strokeLinecap="round" strokeLinejoin="round"/>
+            <path d="M232.138 150.047C240.806 96.0484 204.058 45.2468 150.059 36.5788C96.0605 27.9107 45.2589 64.6586 36.5909 118.658C27.9228 172.656 64.6708 223.458 118.67 232.126C172.669 240.794 223.47 204.046 232.138 150.047Z" stroke="white" strokeWidth="3" strokeMiterlimit="10" strokeLinecap="round" strokeLinejoin="round"/>
+            <path d="M165.793 208.749C206.876 191.385 226.104 144.005 208.741 102.922C191.377 61.8386 143.997 42.6102 102.913 59.9739C61.8303 77.3375 42.6019 124.718 59.9655 165.801C77.3292 206.884 124.71 226.113 165.793 208.749Z" stroke="white" strokeWidth="3" strokeMiterlimit="10" strokeLinecap="round" strokeLinejoin="round"/>
+            <path d="M158.598 191.985C190.427 178.593 205.373 141.935 191.981 110.107C178.59 78.2781 141.931 63.3322 110.103 76.7238C78.2747 90.1154 63.3287 126.774 76.7203 158.602C90.1119 190.43 126.77 205.376 158.598 191.985Z" stroke="white" strokeWidth="3" strokeMiterlimit="10" strokeLinecap="round" strokeLinejoin="round"/>
+            <path d="M151.459 175.19C174.008 165.746 184.633 139.811 175.189 117.261C165.745 94.7119 139.81 84.0875 117.26 93.5312C94.7109 102.975 84.0865 128.91 93.5302 151.46C102.974 174.009 128.91 184.634 151.459 175.19Z" stroke="white" strokeWidth="3" strokeMiterlimit="10" strokeLinecap="round" strokeLinejoin="round"/>
+            <path d="M159.68 140.306C162.967 126.326 154.299 112.328 140.319 109.041C126.338 105.754 112.34 114.422 109.053 128.402C105.766 142.382 114.434 156.38 128.415 159.667C142.395 162.955 156.393 154.286 159.68 140.306Z" stroke="white" strokeWidth="3" strokeMiterlimit="10" strokeLinecap="round" strokeLinejoin="round"/>
+            <path d="M142.098 134.356C142.098 138.628 138.629 142.097 134.357 142.097C130.086 142.097 126.617 138.628 126.617 134.356C126.617 130.084 130.086 126.615 134.357 126.615C138.629 126.615 142.098 130.084 142.098 134.356Z" stroke="white" strokeWidth="3" strokeMiterlimit="10" strokeLinecap="round" strokeLinejoin="round"/>
+          </svg>
+
+        <svg width="84" className={styles.sectionDecorationTwo} height="269" viewBox="0 0 84 269" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M65.1652 152.914C75.4124 88.9564 31.8715 28.8015 -32.0861 18.5543C-96.0437 8.30704 -156.199 51.8479 -166.446 115.806C-176.693 179.763 -133.152 239.918 -69.1945 250.165C-5.23688 260.413 54.918 216.872 65.1652 152.914Z" stroke="white" strokeWidth="3" strokeMiterlimit="10" strokeLinecap="round" strokeLinejoin="round"/>
+          <path d="M47.138 150.047C55.8061 96.0484 19.0582 45.2468 -34.9408 36.5788C-88.9397 27.9107 -139.741 64.6586 -148.409 118.658C-157.077 172.656 -120.329 223.458 -66.3305 232.126C-12.3316 240.794 38.47 204.046 47.138 150.047Z" stroke="white" strokeWidth="3" strokeMiterlimit="10" strokeLinecap="round" strokeLinejoin="round"/>
+          <path d="M-19.2073 208.749C21.8758 191.385 41.1043 144.005 23.7406 102.922C6.37699 61.8387 -41.0035 42.6102 -82.0866 59.9739C-123.17 77.3375 -142.398 124.718 -125.034 165.801C-107.671 206.884 -60.2904 226.113 -19.2073 208.749Z" stroke="white" strokeWidth="3" strokeMiterlimit="10" strokeLinecap="round" strokeLinejoin="round"/>
+          <path d="M-26.4016 191.985C5.42678 178.593 20.3728 141.935 6.98116 110.106C-6.41045 78.2781 -43.0685 63.3321 -74.8969 76.7237C-106.725 90.1154 -121.671 126.773 -108.28 158.602C-94.8881 190.43 -58.23 205.376 -26.4016 191.985Z" stroke="white" strokeWidth="3" strokeMiterlimit="10" strokeLinecap="round" strokeLinejoin="round"/>
+          <path d="M-33.541 175.19C-10.9916 165.746 -0.367226 139.811 -9.81092 117.261C-19.2546 94.7118 -45.1902 84.0875 -67.7397 93.5312C-90.2891 102.975 -100.913 128.91 -91.4698 151.46C-82.0261 174.009 -56.0905 184.634 -33.541 175.19Z" stroke="white" strokeWidth="3" strokeMiterlimit="10" strokeLinecap="round" strokeLinejoin="round"/>
+        </svg>
+
+      </div>
+
     </div>
   )
 }
